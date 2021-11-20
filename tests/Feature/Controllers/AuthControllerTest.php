a93f0526cd8816948b1872fafd9ad8407de7e039
Channing Defoe <channingdefoe@delta.edu>
2021-11-19 23:34:15 -0500

    add basic unit test for jwt endpoint

---
+++ tests/Feature/Controllers/AuthControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+
+class AuthControllerTest extends TestCase
+{
+    public function test_can_get_jwt()
+    {
+        // Arrange
+        User::factory()->create([
+            'email' => 'test@example.org',
+            'password' => bcrypt('secret')
+        ]);
+
+        // Act
+        $response = $this->json('POST', '/api/auth/login', [
+            'email' => 'test@example.org',
+            'password' => 'secret'
+        ]);
+
+        // Assert
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,47 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, RefreshDatabase;
+
+    /**
+     * Send json request as client
+     *
+     * @param string $method
+     * @param string $url
+     * @param array $data
+     * @param array $headers
+     * @param User $user
+     *
+     * @return TestResponse
+     */
+    public function jsonAsUser(string $method, string $url, array $data = [], array $headers = [], User $user = null) : TestResponse
+    {
+        $accessToken = $this->getAccessToken($user);
+
+        return $this->json($method, $url, $data, array_merge([
+            'Authorization' => "Bearer {$accessToken}",
+        ], $headers));
+    }
+
+    /**
+     * Get access token
+     * 
+     * @param User $user
+     * 
+     * @return string
+     */
+    public function getAccessToken(User $user = null) : string
+    {
+        if (!$user) {
+            $user = User::factory()->create();
+        }
+
+        return auth()->login($user);
+    }
 }
